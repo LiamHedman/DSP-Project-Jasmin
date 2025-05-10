258b04ff4ec9645ff3c7f9cea245b83078842bc9
Liam Hedman <liamhedman@gmail.com>
2025-05-10 13:31:03 +0200

    UI ändringar
    Har tagit bort den starka gröna bakgrunden på create-post-siddan. Har lagt till BoomerBorrow-loggan på startskärmen. Har ändrat färgsynken på bakgrunden men lyckas inte ändra färgen på knapparna. Har lagt till en fil i assets som tillåter importering av bilder i andra filer.

--- BoomerBorrow/app/(tabs)/create_posts.tsx
+++ BoomerBorrow/app/(tabs)/create_posts.tsx
@@ -113,7 +113,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'green'
+        backgroundColor: 'white'
     },
     input: {
         height: 40,
